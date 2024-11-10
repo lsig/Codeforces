ebf9bba95a0465ecb0c6df1bb2b28bed24301ac8
Lsig <logis21@ru.is>
2024-11-10 10:35:42 -0500

    feat: Upload template

--- templates/default.cpp
+++ templates/default.cpp
@@ -39,7 +39,7 @@ ostream &operator<<(ostream &os, const T_container &v) {
 	for (const T &x : v) { os << sep << x, sep = ", "; }
 	return os << '}';
 }
-void dbg_out() { cerr << '\n'; }
+void dbg_out() { cerr << endl; }
 template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {
 	cerr << ' ' << H;
 	dbg_out(T...);
@@ -63,12 +63,10 @@ const ll INF = 1e9;
 const ld EPS = 1e-9;
 
 void solve() {
-	// Each test case reads ONE number
 	int x = 0;
 	cin >> x;
 	dbg(x);
 
-	// Process the number (in this example, just print it)
 	cout << "Number: " << x << endl;
 }
 
